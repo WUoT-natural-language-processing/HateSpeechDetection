59fa701d5b28fedcf606c6e3ab5d9f42c1817136
Kuba <kuba.urban01@gmail.com>
2024-03-28 13:44:43 +0100

    Init

---
+++ main.py
@@ -0,0 +1,34 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics import accuracy_score, classification_report
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import make_pipeline
+from sklearn.svm import SVC
+import pandas as pd
+
+df = pd.read_csv('labeled_data.csv')
+
+y = df.loc[:, 'class']
+X = df.drop(['class'], axis=1)
+
+# Podział danych na zbiór treningowy i testowy
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=12)
+
+X_train = X_train["tweet"]
+X_test = X_test["tweet"]
+
+# Definicja klasyfikatora SVM z wykorzystaniem TF-IDF
+classifier = make_pipeline(TfidfVectorizer(), SVC(kernel='linear'))
+
+# Trenowanie klasyfikatora
+classifier.fit(X_train, y_train)
+
+# Predykcja na zbiorze testowym
+y_pred = classifier.predict(X_test)
+
+# Ocena wydajności klasyfikatora
+accuracy = accuracy_score(y_test, y_pred)
+report = classification_report(y_test, y_pred)
+
+print("Accuracy:", accuracy)
+print("Classification Report:")
+print(report)
